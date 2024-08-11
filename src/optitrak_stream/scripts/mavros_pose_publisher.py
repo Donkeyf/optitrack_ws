ce3b7b7de4bfd3bebc7621d78205d5825ad6d3e9
jamesolee <jollee8013@gmail.com>
2024-08-11 14:34:50 +1000

    added posesubscriber class

--- src/optitrak_stream/scripts/mavros_pose_publisher.py
+++ src/optitrak_stream/scripts/mavros_pose_publisher.py
@@ -3,15 +3,19 @@ import rospy
 from mavros_msgs.msg import Mavlink
 from geometry_msgs.msg import PoseStamped
 
-class OptitrackSubscriber:
+class PoseSubscriber:
     def __init__(self):
-        self.received_data = None  # Variable to store the received data
+        self.X = None  # Variable to store the received data
+        self.Y = None  # Variable to store the received data
+        self.Z = None  # Variable to store the received data
         # Subscriber to a topic
         self.subscriber = rospy.Subscriber("/vrpn_client_node/Robot_1/pose", PoseStamped, self.callback)
     
     def callback(self, data):
         # Store the data in the variable
-        self.received_data = data.data
+        self.X = data.pose.position.x
+        self.Y = data.pose.position.y
+        self.Z = data.pose.position.z
         rospy.loginfo(f"Data received: {self.received_data}")
 
     def get_data(self):
@@ -22,11 +26,8 @@ if __name__ == '__main__':
     rospy.loginfo("optitrak data is streaming")
 
     pub = rospy.Publisher("/mavlink/to", Mavlink, queue_size = 10)
-    opt_sub = OptitrackSubscriber()
+    sub = PoseSubscriber()
     
-    X = opt_sub.get_data().pose.position.x
-    Y = opt_sub.get_data().pose.position.y
-    Z = opt_sub.get_data().pose.position.z
 
     #refresh rate at which information is published
     rate = rospy.Rate(15)
@@ -34,7 +35,7 @@ if __name__ == '__main__':
     while not rospy.is_shutdown():
         #TODO stream optitrak data to RPi
 
-        print(f'x = {X}, y = {Y}, z = {Z}')
+        print(f'x = {sub.X}, y = {sub.Y}, z = {sub.Z}')
 
         msg = Mavlink()
         #TODO fill in required mavlink msg data
