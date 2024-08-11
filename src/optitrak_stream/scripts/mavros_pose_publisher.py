760bb39ff6635fe75d87c7f1e488269002e9672c
DilonH <dilonhewamanna@outlook.com>
2024-08-11 13:51:22 +1000

    Update mavros_pose_publisher.py

--- src/optitrak_stream/scripts/mavros_pose_publisher.py
+++ src/optitrak_stream/scripts/mavros_pose_publisher.py
@@ -1,12 +1,17 @@
 #!usr/bin/env python3
 import rospy
 from mavros_msgs.msg import Mavlink
+from geometry_msgs.msg import PoseStamped
+
+def receive_pose(pose):
+    print(pose)
 
 if __name__ == '__main__':
     rospy.init_node("mavros_pose_publisher")
     rospy.loginfo("optitrak data is streaming")
 
     pub = rospy.Publisher("/mavlink/to", Mavlink, queue_size = 10)
+    sub = rospy.Subscriber("/Robot_1/pose", PoseStamped, receive_pose)
 
     #refresh rate at which information is published
     rate = rospy.Rate(15)
