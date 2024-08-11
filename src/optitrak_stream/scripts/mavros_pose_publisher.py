c8b7d76ed367b8168a3ce50a525c1078d29fa65b
jamesolee <jollee8013@gmail.com>
2024-08-11 15:03:31 +1000

    added posepublisher

--- src/optitrak_stream/scripts/mavros_pose_publisher.py
+++ src/optitrak_stream/scripts/mavros_pose_publisher.py
@@ -5,15 +5,17 @@ from geometry_msgs.msg import PoseStamped
 
 class PoseSubscriber:
     def __init__(self):
+        self.pose = None
         self.X = None  # Variable to store the received data
         self.Y = None  # Variable to store the received data
         self.Z = None  # Variable to store the received data
         self.Q = []
         # Subscriber to a topic
-        self.subscriber = rospy.Subscriber("/vrpn_client_node/Robot_1/pose", PoseStamped, self.callback)
+        self.subscriber = rospy.Subscriber("/vrpn_client_node/FASTR/pose", PoseStamped, self.callback)
     
     def callback(self, data):
         # Store the data in the variable
+        self.pose_stamped = data
         self.X = data.pose.position.x
         self.Y = data.pose.position.y
         self.Z = data.pose.position.z
@@ -24,16 +26,24 @@ class PoseSubscriber:
 
     def get_data(self):
         return self.received_data
+    
+class PosePublisher:
+    def __init__(self):
+        mocap_publisher = rospy.Publisher('/mavros/mocap/pose', PoseStamped, queue_size=10)
+        #refresh rate at which information is published
+        
+
+    def send_att_pos_mocap(self,pose_stamped):
+        # publish the PoseStamped message
+        self.mocap_publisher.publish(pose_stamped)
 
 if __name__ == '__main__':
     rospy.init_node("mavros_pose_publisher")
     rospy.loginfo("optitrak data is streaming")
 
-    pub = rospy.Publisher("/mavlink/to", Mavlink, queue_size = 10)
+    pub = PosePublisher()
     sub = PoseSubscriber()
-    
 
-    #refresh rate at which information is published
     rate = rospy.Rate(15)
 
     while not rospy.is_shutdown():
@@ -41,9 +51,10 @@ if __name__ == '__main__':
 
         print(f'x = {sub.X}, y = {sub.Y}, z = {sub.Z}, q = {sub.Q}')
 
-        msg = Mavlink()
-        #TODO fill in required mavlink msg data
+        pub.send_att_pos_mocap(sub.pose_stamped)
 
-        pub.publish(msg)
+        # TODO for SLAM later
+        # msg = Mavlink()
+        # fill in required mavlink msg data
 
         rate.sleep()
