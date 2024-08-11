6caf53cf7dda015d229f0904263e1384320731cf
jamesolee <jollee8013@gmail.com>
2024-08-11 14:26:10 +1000

    Changed subscriber to OptitrackSubscriber class

--- src/optitrak_stream/scripts/mavros_pose_publisher.py
+++ src/optitrak_stream/scripts/mavros_pose_publisher.py
@@ -6,18 +6,38 @@ from geometry_msgs.msg import PoseStamped
 def receive_pose(pose):
     print(pose)
 
+class OptitrackSubscriber:
+    def __init__(self):
+        self.received_data = None  # Variable to store the received data
+        # Subscriber to a topic
+        self.subscriber = rospy.Subscriber("/vrpn_client_node/Robot_1/pose", PoseStamped, receive_pose)
+    
+    def callback(self, data):
+        # Store the data in the variable
+        self.received_data = data.data
+        rospy.loginfo(f"Data received: {self.received_data}")
+
+    def get_data(self):
+        return self.received_data
+
 if __name__ == '__main__':
     rospy.init_node("mavros_pose_publisher")
     rospy.loginfo("optitrak data is streaming")
 
     pub = rospy.Publisher("/mavlink/to", Mavlink, queue_size = 10)
-    sub = rospy.Subscriber("/vrpn_client_node/Robot_1/pose", PoseStamped, receive_pose)
+    opt_sub = OptitrackSubscriber()
+    
+    X = opt_sub.get_data.pose.position.x
+    Y = opt_sub.get_data.pose.position.y
+    Z = opt_sub.get_data.pose.position.z
 
     #refresh rate at which information is published
     rate = rospy.Rate(15)
 
     while not rospy.is_shutdown():
         #TODO stream optitrak data to RPi
+
+        print(f'x = {X}, y = {Y}, z = {Z}')
 
         msg = Mavlink()
         #TODO fill in required mavlink msg data
